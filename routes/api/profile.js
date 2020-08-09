392c6d835add3939cee291c85ad3e4515a512130
Joshua Nho <joshnho@gmail.com>
2020-08-09 03:45:33 -0400

    changed to Basic Authentication from using OAuth in query parameters

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -288,7 +288,8 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 router.get('/github/:username', async (req, res) => {
     try {
         const options = {
-            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`,
+            Authorization: `token ${config.get('githubSecret')}`,
             method: 'GET',
             headers: { 'user-agent': 'node.js' }
         }
