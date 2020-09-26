13ef54afaaf6e2fd7a405b395a6e70441125a6cd
Joshua Nho <joshnho@gmail.com>
2020-09-26 19:55:26 -0400

    added funcationality to add education and experiences to profile

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -46,11 +46,11 @@ const CreateProfile = ({ createProfile, history }) => {
 
     return (
         <Fragment>
-            <h1 class="large text-primary">
+            <h1 className="large text-primary">
                 Create Your Profile
             </h1>
-            <p class="lead">
-                <i class="fas fa-user"></i> 
+            <p className="lead">
+                <i className="fas fa-user"></i> 
                 Let's get some information to make your
                 profile stand out
             </p>
