e2dc69ead841e4dc72f00e87b1ba6053c0d1bea5
Joshua Nho <joshnho@gmail.com>
2020-09-27 08:04:12 -0400

    rearranged texts

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -12,19 +12,17 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
 
     return (
         <Fragment>
-            { loading ? <Spinner /> : <Fragment>
-                <h1 className='large text-primary'>Developers</h1>
-                <p className='lead'>
-                    <i className='fab fa-connectdevelop'></i>Browse and connect with Developers
-                </p>
-                <div className='profiles'>
-                    {profiles.length > 0 ? (
-                        profiles.map(profile => (
-                            <ProfileItem key={profile._id} profile={profile} />
-                        ))
-                    ) : <h4>No Profiles Found...</h4>}
-                </div>
-            </Fragment>}
+            <h1 className='large text-primary'>Developers</h1>
+            <p className='lead'>
+                <i className='fab fa-connectdevelop'></i>Browse and connect with Developers
+            </p>
+            { loading ? <Spinner /> : <div className='profiles'>
+                {profiles.length > 0 ? (
+                    profiles.map(profile => (
+                        <ProfileItem key={profile._id} profile={profile} />
+                    ))
+                ) : <h4>No Profiles Found...</h4>}
+            </div>}
         </Fragment>
     )
 }
