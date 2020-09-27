bffda0ba872fe377b826bbd718046a123ef35b70
Joshua Nho <joshnho@gmail.com>
2020-09-27 02:41:52 -0400

    cleared up console warnings

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -1,8 +1,8 @@
 import React, { Fragment, useState } from 'react'
-import { Link, withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { addEducation, addExperience } from '../../actions/profile'
+import { addEducation } from '../../actions/profile'
 
 const AddEducation = ({ addEducation, history }) => {
     const [formData, setFormData] = useState({
@@ -78,4 +78,4 @@ AddEducation.propTypes = {
     addEducation: PropTypes.func.isRequired
 }
 
-export default connect(null, { addEducation })(AddEducation)
+export default connect(null, { addEducation })(withRouter(AddEducation))
