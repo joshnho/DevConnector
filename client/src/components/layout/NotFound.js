b88c12a5c7fdbfea710b5df458ed2d9da673e28e
Joshua Nho <joshnho@gmail.com>
2020-09-26 23:44:11 -0400

    fixed typo

--- client/src/components/layout/NotFound.js
+++ client/src/components/layout/NotFound.js
@@ -4,9 +4,10 @@ const NotFound = () => {
     return (
         <Fragment>
             <h1 className="x-large text-primary">
-                <i className="fas fa-exclamation-triangle"> Page Not Found</i>
+                <i className="fas fa-exclamation-triangle"> Page Not Found</i>{' '}
+                <i className="fas fa-exclamation-triangle" />
             </h1>
-            <p className="large">Sorry, this page does not exists</p>
+            <p className="large">Sorry, this page does not exist</p>
         </Fragment>
     )
 }
