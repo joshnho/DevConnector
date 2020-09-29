cabb18435a5ff9cd0076dccca8ad41db921d05f6
Joshua Nho <joshnho@gmail.com>
2020-09-28 21:08:23 -0400

    started ProfileTop component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
-import { getProfileById, getProfiles } from '../../actions/profile'
+import ProfileTop from './ProfileTop'
+import { getProfileById } from '../../actions/profile'
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
     useEffect(() => {
@@ -17,6 +18,10 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
                     Back to Profiles
                 </Link>
                 {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>)}
+            <div className="profile-grid my-1">
+                <ProfileTop profile={profile} />
+            </div>
+
             </Fragment>}
         </Fragment>
     )
