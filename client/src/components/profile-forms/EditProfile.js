13cf5261b6250e56c736694fe911bb19ee45b634
Joshua Nho <joshnho@gmail.com>
2020-09-29 16:40:25 -0400

    fixed console warnings and fixed issue where hrefs would append to end of current page url if user doesn't include prefix 'https://' to website or social media links

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -68,8 +68,8 @@ const EditProfile = ({ profile: { profile, loading }, createProfile, getCurrentP
             <h1 className="large text-primary">
                 Create Your Profile
             </h1>
-            <p class="lead">
-                <i class="fas fa-user"></i> 
+            <p className="lead">
+                <i className="fas fa-user"></i> 
                 Let's get some information to make your
                 profile stand out
             </p>
@@ -143,27 +143,27 @@ const EditProfile = ({ profile: { profile, loading }, createProfile, getCurrentP
                 {displaySocialInputs && <Fragment>
                     <div className="form-group social-input">
                         <i className="fab fa-twitter fa-2x"></i>
-                        <input type="text" placeholder="Twitter URL" name="twitter" value={twitter} onChange={(e) => onChange(e)}/>
+                        <input type="text" placeholder="Twitter URL" name="twitter" value={twitter || ''} onChange={(e) => onChange(e)}/>
                     </div>
 
                     <div className="form-group social-input">
                         <i className="fab fa-facebook fa-2x"></i>
-                        <input type="text" placeholder="Facebook URL" name="facebook" value={facebook} onChange={(e) => onChange(e)}/>
+                        <input type="text" placeholder="Facebook URL" name="facebook" value={facebook || ''} onChange={(e) => onChange(e)}/>
                     </div>
 
                     <div className="form-group social-input">
                         <i className="fab fa-youtube fa-2x"></i>
-                        <input type="text" placeholder="YouTube URL" name="youtube" value={youtube} onChange={(e) => onChange(e)}/>
+                        <input type="text" placeholder="YouTube URL" name="youtube" value={youtube || ''} onChange={(e) => onChange(e)}/>
                     </div>
 
                     <div className="form-group social-input">
                         <i className="fab fa-linkedin fa-2x"></i>
-                        <input type="text" placeholder="Linkedin URL" name="linkedin" value={linkedin} onChange={(e) => onChange(e)}/>
+                        <input type="text" placeholder="Linkedin URL" name="linkedin" value={linkedin || ''} onChange={(e) => onChange(e)}/>
                     </div>
 
                     <div className="form-group social-input">
                         <i className="fab fa-instagram fa-2x"></i>
-                        <input type="text" placeholder="Instagram URL" name="instagram" value={instagram} onChange={(e) => onChange(e)}/>
+                        <input type="text" placeholder="Instagram URL" name="instagram" value={instagram || ''} onChange={(e) => onChange(e)}/>
                     </div>
                 </Fragment>}
 
--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -10,6 +10,13 @@ const ProfileTop = ({ profile: {
     user: { name, avatar}
 } }) => {
 
+    const urlToRender = (link) => {
+        if (!link.match(/^[a-zA-Z]+:\/\//)){
+            return '//' + link;
+        }
+        return link;
+    }
+
     return (
         <div className="profile-top bg-primary p-2">
             <img
@@ -23,25 +30,35 @@ const ProfileTop = ({ profile: {
             <p>{location && <span>{location}</span>}</p>
             <div className="icons my-1">
                 {website && (
-                    <a href={`${website}`} target="_blank" rel="noopener noreferrer">
+                    <a href={urlToRender(website)} target="_blank" rel="noopener noreferrer">
                         <i className="fas fa-globe fa-2x"></i>
                     </a>
                 )}
-                {/* <a href="#" target="_blank" rel="noopener noreferrer">
-                    <i class="fab fa-twitter fa-2x"></i>
+                {social && social.twitter && (
+                    <a href={urlToRender(social.twitter)} target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-twitter fa-2x"></i>
                 </a>
-                <a href="#" target="_blank" rel="noopener noreferrer">
-                    <i class="fab fa-facebook fa-2x"></i>
+                )}
+                {social && social.facebook && (
+                    <a href={urlToRender(social.facebook)} target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-facebook fa-2x"></i>
                 </a>
-                <a href="#" target="_blank" rel="noopener noreferrer">
-                    <i class="fab fa-linkedin fa-2x"></i>
+                )}
+                {social && social.linkedin && (
+                    <a href={urlToRender(social.linkedin)} target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-linkedin fa-2x"></i>
                 </a>
-                    <a href="#" target="_blank" rel="noopener noreferrer">
-                    <i class="fab fa-youtube fa-2x"></i>
+                )}
+                {social && social.youtube && (
+                    <a href={urlToRender(social.youtube)} target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-youtube fa-2x"></i>
                 </a>
-                <a href="#" target="_blank" rel="noopener noreferrer">
-                    <i class="fab fa-instagram fa-2x"></i>
-                </a> */}
+                )}
+                {social && social.instagram && (
+                    <a href={urlToRender(social.instagram)} target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-instagram fa-2x"></i>
+                </a>
+                )}
             </div>
         </div>
     )
