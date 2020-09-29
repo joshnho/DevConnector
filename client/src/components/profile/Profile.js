868662e12b3eab009f9ae1e2fc0176f475be784d
Joshua Nho <joshnho@gmail.com>
2020-09-29 16:54:47 -0400

    finished ProfileTop component and started/finished ProfileAbout component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import ProfileTop from './ProfileTop'
+import ProfileAbout from './ProfileAbout'
 import { getProfileById } from '../../actions/profile'
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
@@ -20,6 +21,7 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
                 {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>)}
             <div className="profile-grid my-1">
                 <ProfileTop profile={profile} />
+                <ProfileAbout profile={profile} />
             </div>
 
             </Fragment>}
