da3ae7e0375e53c9d69f48b834f5fcd34e0a363c
Joshua Nho <joshnho@gmail.com>
2020-10-04 00:39:13 -0400

    created action and reducer for post and created private route to posts component

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -10,6 +10,7 @@ import AddExperience from '../profile-forms/AddExperience'
 import AddEducation from '../profile-forms/AddEducation'
 import Profiles from '../profiles/Profiles'
 import Profile from '../profile/Profile'
+import Posts from '../posts/Posts'
 import NotFound from '../layout/NotFound'
 import PrivateRoute from '../routing/PrivateRoute'
 
@@ -27,6 +28,7 @@ const Routes = () => {
                 <PrivateRoute exact path="/edit-profile" component={EditProfile} />
                 <PrivateRoute exact path="/add-experience" component={AddExperience} />
                 <PrivateRoute exact path="/add-education" component={AddEducation} />
+                <PrivateRoute exact path="/posts" component={Posts} />
                 <Route component={NotFound} />
             </Switch>
         </section>
