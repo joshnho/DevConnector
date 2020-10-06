eb0e7493b168d5d4eec6b94c6d0cf235de176c94
Joshua Nho <joshnho@gmail.com>
2020-10-06 08:03:46 -0400

    created action, reducer, and form to add comments

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,4 @@
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, UPDATE_DISLIKES, DELETE_POST, ADD_POST, GET_POST } from '../actions/types'
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, UPDATE_DISLIKES, DELETE_POST, ADD_POST, GET_POST, ADD_COMMENT, REMOVE_COMMENT } from '../actions/types'
 
 const initialState = {
     posts: [],
@@ -51,6 +51,21 @@ export default function(state = initialState, action) {
             return {
                 ...state,
                 posts: state.posts.map(post => post._id === payload.id ? { ...post, dislikes: payload.dislikes } : post),
+                loading: false
+            }
+        case ADD_COMMENT:
+            return {
+                ...state,
+                post: { ...state.post, comments: payload },
+                loading: false
+            }
+        case REMOVE_COMMENT:
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    comments: state.post.comments.filter(comment => comment._id !== payload)
+                },
                 loading: false
             }
         default:
