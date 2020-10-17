a64e176b0447cb8f79d99cce12623fc8e99130f9
Joshua Nho <joshnho@gmail.com>
2020-10-17 18:08:14 -0400

    fixed minor bug

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -18,11 +18,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
             Back To Posts
         </Link>
         <PostItem post={post} showActions={false}/>
-        <CommentForm postId={post._id} />
         <div className='comments'>
             {post.comments.map(comment => (
                 <CommentItem key={comment._id} comment={comment} postId={post._id} />
-            ))}
+                ))}
+        <CommentForm postId={post._id} />
         </div>
     </Fragment>
     
